7f6589703dafac4acc12b5d0a9975899999c3403
Kalin-Rudnicki <deaddorks@gmail.com>
2021-03-18 14:24:22 -0500

    Finished MatchResult

--- build.sbt
+++ build.sbt
@@ -9,11 +9,11 @@ lazy val `klib-webServer` =
     .settings(
       name := "klib-webserver",
       organization := "kalin-rudnicki",
-      version := "0.0.8",
+      version := "0.0.9",
       unmanagedSourceDirectories in Compile +=
         baseDirectory.value / "shared" / "main" / "scala",
       libraryDependencies ++= Seq(
-        "kalin-rudnicki" %%% "klib-core" % "0.1.4",
+        "kalin-rudnicki" %%% "klib-core" % "0.1.5",
         "com.lihaoyi" %%% "scalatags" % "0.9.2",
         "io.circe" %%% "circe-core" % CirceVersion,
         "io.circe" %%% "circe-generic" % CirceVersion,
--- klib-webServer/jvm/src/main/scala/klib/webServer/MatchResult.scala
+++ klib-webServer/jvm/src/main/scala/klib/webServer/MatchResult.scala
@@ -1,27 +1,53 @@
 package klib.webServer
 
-import java.io.File
-
-import io.circe._, parser._, generic.auto._
-import jakarta.servlet.http.Cookie
-import scalatags.Text.Frag
-
-import klib.Implicits._
-import klib.fp.types._
-
-sealed trait MatchResult
-object MatchResult {
-
-  case object FailedMatch extends MatchResult
-
-}
+import klib.fp.typeclass._
 
 // =====|  |=====
 
-sealed trait MatchResult2[+T]
-object MatchResult2 {
+sealed trait MatchResult[+T]
+object MatchResult {
 
-  final case class Continue[+T](value: T) extends MatchResult2[T]
-  final case class Done(result: Response) extends MatchResult2[Nothing]
+  final case class Continue[+T](value: T) extends MatchResult[T]
+  final case class Done(result: Response) extends MatchResult[Nothing]
+
+  //
+
+  implicit val matchResultMonad: Monad[MatchResult] =
+    new Monad[MatchResult] {
+
+      override def map[A, B](t: MatchResult[A], f: A => B): MatchResult[B] =
+        t match {
+          case Continue(value) =>
+            Continue(f(value))
+          case done @ Done(_) =>
+            done
+        }
+
+      // NOTE : Don't really like the whole apply thing, but want map & flatMap
+      override def apply[A, B](t: MatchResult[A], f: MatchResult[A => B]): MatchResult[B] =
+        t match {
+          case Continue(tValue) =>
+            f match {
+              case Continue(fValue) =>
+                Continue(fValue(tValue))
+              case done @ Done(_) =>
+                done
+            }
+          case done @ Done(_) =>
+            done
+        }
+
+      override def pure[A](a: => A): MatchResult[A] =
+        Continue(a)
+
+      override def flatten[A](t: MatchResult[MatchResult[A]]): MatchResult[A] =
+        t match {
+          case Continue(value) =>
+            value
+          case done @ Done(_) =>
+            done
+        }
+
+    }
 
 }
