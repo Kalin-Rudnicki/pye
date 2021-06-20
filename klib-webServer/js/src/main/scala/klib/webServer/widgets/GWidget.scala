11d42642af6578d4af25f8450202b1ac6c5a47c0
Kalin-Rudnicki <kalin.rudnicki@gmail.com>
2021-06-20 12:24:21 -0500

    Altered internal permissions of GWidget

--- klib-webServer/js/src/main/scala/klib/webServer/widgets/GWidget.scala
+++ klib-webServer/js/src/main/scala/klib/webServer/widgets/GWidget.scala
@@ -4,14 +4,14 @@ import klib.fp.types._
 import org.scalajs.dom.raw.HTMLElement
 
 sealed abstract class GWidget[Value, S] private (val state: S) {
-  protected val stateToValue: S => ?[Value]
-  protected val stateToNode: S => HTMLElement
+  val stateToValue: S => ?[Value]
+  val stateToNode: S => HTMLElement
 
-  private var _node: HTMLElement = stateToNode(state)
+  private final var _node: HTMLElement = stateToNode(state)
 
-  def node: HTMLElement = _node
-  def value: ?[Value] = stateToValue(state)
-  def reRender(): Unit = {
+  final def node: HTMLElement = _node
+  final def value: ?[Value] = stateToValue(state)
+  final def reRender(): Unit = {
     val newNode = stateToNode(state)
     _node.parentNode.replaceChild(newNode, _node)
     _node = newNode
@@ -22,8 +22,8 @@ object GWidget {
 
   def apply[Value, S](_stateToValue: S => ?[Value])(_stateToNode: S => HTMLElement): GWidgetF[Value, S] = { s =>
     new GWidget[Value, S](s) {
-      override protected val stateToValue: S => ?[Value] = _stateToValue
-      override protected val stateToNode: S => HTMLElement = _stateToNode
+      override final val stateToValue: S => ?[Value] = _stateToValue
+      override final val stateToNode: S => HTMLElement = _stateToNode
     }
   }
 
