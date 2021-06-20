f2b1f0559ed03195ea536d7b7191a823a827e7e9
Kalin-Rudnicki <kalin.rudnicki@gmail.com>
2021-06-20 14:57:38 -0500

    Added helpers to Widget

--- klib-webServer/js/src/main/scala/klib/webServer/widgets/Widget.scala
+++ klib-webServer/js/src/main/scala/klib/webServer/widgets/Widget.scala
@@ -1,5 +1,6 @@
 package klib.webServer.widgets
 
+import klib.Implicits._
 import klib.fp.types._
 import scalatags.JsDom.Frag
 
@@ -13,16 +14,36 @@ final class Widget[Value, S] private (
 
 object Widget {
 
-  final class Builder[Value, S](
+  final class Builder[Value, S] private[Widget] (
       val stateToValue: S => ?[Value],
       val stateToNode: S => Frag,
   ) {
+
     def apply(s: S): Widget[Value, S] =
       new Widget[Value, S](
         state = s,
         node = stateToNode(s),
         _value = stateToValue(s),
       )
+
+    def wrap(in: Frag => Frag): Builder[Value, S] =
+      new Builder[Value, S](
+        stateToValue = stateToValue,
+        stateToNode = s => in(stateToNode(s)),
+      )
+
+    def map[Value2](f: Value => Value2): Builder[Value2, S] =
+      new Builder[Value2, S](
+        stateToValue = stateToValue(_).map(f),
+        stateToNode = stateToNode,
+      )
+
+    def flatMap[Value2](f: Value => ?[Value2]): Builder[Value2, S] =
+      new Builder[Value2, S](
+        stateToValue = stateToValue(_).flatMap(f),
+        stateToNode = stateToNode,
+      )
+
   }
 
   def apply[Value, S](stateToValue: S => ?[Value])(stateToNode: S => Frag): Builder[Value, S] =
