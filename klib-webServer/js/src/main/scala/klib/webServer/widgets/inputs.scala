58540afe9a9aa77d26ed6ba6f6deba5e2dde37d4
Kalin-Rudnicki <kalin.rudnicki@gmail.com>
2021-06-23 22:40:08 -0500

    Label errors

--- klib-webServer/js/src/main/scala/klib/webServer/widgets/inputs.scala
+++ klib-webServer/js/src/main/scala/klib/webServer/widgets/inputs.scala
@@ -38,49 +38,51 @@ trait inputs {
       id: String,
       decorators: Decorators,
   ): Widget.Builder[Maybe[String], Var[String]] =
-    Widget.Builder[Maybe[String], Var[String]] { s =>
-      val _label =
-        JD.label(JD.id := s"$id-label", `for` := id, `class` := s"kws:${_type} kws:field-label", label)(
-          decorators.labelModifiers,
-        )
-      val _input = __input(JD.id := id, `class` := s"kws:${_type}")(decorators.inputModifiers).render
+    Widget
+      .Builder[Maybe[String], Var[String]] { s =>
+        val _label =
+          JD.label(JD.id := s"$id-label", `for` := id, `class` := s"kws:${_type} kws:field-label", label)(
+            decorators.labelModifiers,
+          )
+        val _input = __input(JD.id := id, `class` := s"kws:${_type}")(decorators.inputModifiers).render
 
-      val _errors =
-        span(JD.id := s"$id-errors", `class` := s"kws:${_type}-errors kws:field-errors")(decorators.errorsModifiers)
+        val _errors =
+          span(JD.id := s"$id-errors", `class` := s"kws:${_type}-errors kws:field-errors")(decorators.errorsModifiers)
 
-      val prevOnKeyPress = _input.onkeypress
-      _input.onkeypress = { e =>
-        if (prevOnKeyPress != null)
-          prevOnKeyPress(e)
-        if (!e.defaultPrevented) {
-          if (filterSubmit(e)) {
-            e.preventDefault()
-            s.value = _input.value
-            _input.dispatchEvent(submitEvent)
+        val prevOnKeyPress = _input.onkeypress
+        _input.onkeypress = { e =>
+          if (prevOnKeyPress != null)
+            prevOnKeyPress(e)
+          if (!e.defaultPrevented) {
+            if (filterSubmit(e)) {
+              e.preventDefault()
+              s.value = _input.value
+              _input.dispatchEvent(submitEvent)
+            }
           }
         }
-      }
 
-      val prevOnBlur = _input.onblur
-      _input.onblur = { e =>
-        if (prevOnBlur != null)
-          prevOnBlur(e)
-        if (!e.defaultPrevented) {
-          s.value = _input.value
+        val prevOnBlur = _input.onblur
+        _input.onblur = { e =>
+          if (prevOnBlur != null)
+            prevOnBlur(e)
+          if (!e.defaultPrevented) {
+            s.value = _input.value
+          }
         }
+
+        _input.value = s.value
+
+        Widget(
+          span(JD.id := s"$id-container", `class` := s"kws:${_type}-container kws:field-container")(
+            _label,
+            _input,
+            _errors,
+          )(decorators.containerModifiers).render,
+          s.value.ensure(_.nonEmpty).pure[?],
+        )
       }
-
-      _input.value = s.value
-
-      Widget(
-        span(JD.id := s"$id-container", `class` := s"kws:${_type}-container kws:field-container")(
-          _label,
-          _input,
-          _errors,
-        )(decorators.containerModifiers).render,
-        s.value.ensure(_.nonEmpty).pure[?],
-      )
-    }
+      .labelErrors(label)
 
   def input(
       label: String,
@@ -123,70 +125,72 @@ trait inputs {
       onChange: Maybe[T => Unit] = None,
       decorators: Decorators = Decorators(),
   ): Widget.Builder[Maybe[T], Var[Maybe[T]]] =
-    Widget.Builder[Maybe[T], Var[Maybe[T]]] { s =>
-      object selected {
-        private var _value: Maybe[(T, dom.html.Span)] = None
+    Widget
+      .Builder[Maybe[T], Var[Maybe[T]]] { s =>
+        object selected {
+          private var _value: Maybe[(T, dom.html.Span)] = None
 
-        def value: Maybe[(T, dom.html.Span)] = _value
-        def value_=(v: Maybe[(T, dom.html.Span)]): Unit = {
-          s.value = v.map(_._1)
+          def value: Maybe[(T, dom.html.Span)] = _value
+          def value_=(v: Maybe[(T, dom.html.Span)]): Unit = {
+            s.value = v.map(_._1)
 
-          _value.foreach(_._2.classList.remove("kws:radio-group__button--selected"))
-          v.foreach(_._2.classList.add("kws:radio-group__button--selected"))
-          _value = v
-        }
-      }
-
-      val optionNodes: Array[dom.html.Span] =
-        options.zipWithIndex.map {
-          case ((label, t), i) =>
-            val n =
-              span(
-                `class` := List(
-                  List(
-                    "clickable",
-                    "kws:radio-group__button",
-                  ),
-                  (i == 0).maybe("kws:radio-group__button--first").toList,
-                  (i == options.length - 1).maybe("kws:radio-group__button--last").toList,
-                ).flatten.mkString(" "),
-              )(label).render
-
-            n.onclick = { _ =>
-              val newVal = (t, n).some
-              if (newVal != selected.value) {
-                selected.value = (t, n).some
-                onChange.foreach(_(t))
-              }
-            }
-
-            s.value.foreach { s =>
-              if (s == t) {
-                selected.value = (t, n).some
-              }
-            }
-
-            n
+            _value.foreach(_._2.classList.remove("kws:radio-group__button--selected"))
+            v.foreach(_._2.classList.add("kws:radio-group__button--selected"))
+            _value = v
+          }
         }
 
-      val _label =
-        JD.label(JD.id := s"$id-label", `for` := id, `class` := "kws:radio-group-label kws:field-label", label)(
-          decorators.labelModifiers,
+        val optionNodes: Array[dom.html.Span] =
+          options.zipWithIndex.map {
+            case ((label, t), i) =>
+              val n =
+                span(
+                  `class` := List(
+                    List(
+                      "clickable",
+                      "kws:radio-group__button",
+                    ),
+                    (i == 0).maybe("kws:radio-group__button--first").toList,
+                    (i == options.length - 1).maybe("kws:radio-group__button--last").toList,
+                  ).flatten.mkString(" "),
+                )(label).render
+
+              n.onclick = { _ =>
+                val newVal = (t, n).some
+                if (newVal != selected.value) {
+                  selected.value = (t, n).some
+                  onChange.foreach(_(t))
+                }
+              }
+
+              s.value.foreach { s =>
+                if (s == t) {
+                  selected.value = (t, n).some
+                }
+              }
+
+              n
+          }
+
+        val _label =
+          JD.label(JD.id := s"$id-label", `for` := id, `class` := "kws:radio-group-label kws:field-label", label)(
+            decorators.labelModifiers,
+          )
+        val _input =
+          span(`class` := "kws:radio-group", optionNodes)(decorators.inputModifiers).render
+        val _errors =
+          span(JD.id := s"$id-errors", `class` := "kws:radio-group-errors kws:field-errors")(decorators.errorsModifiers)
+
+        Widget[Maybe[T]](
+          node = span(JD.id := s"$id-container", `class` := "kws:radio-group-container kws:field-container")(
+            _label,
+            _input,
+            _errors,
+          )(decorators.containerModifiers).render,
+          s.value.pure[?],
         )
-      val _input =
-        span(`class` := "kws:radio-group", optionNodes)(decorators.inputModifiers).render
-      val _errors =
-        span(JD.id := s"$id-errors", `class` := "kws:radio-group-errors kws:field-errors")(decorators.errorsModifiers)
-
-      Widget[Maybe[T]](
-        node = span(JD.id := s"$id-container", `class` := "kws:radio-group-container kws:field-container")(
-          _label,
-          _input,
-          _errors,
-        )(decorators.containerModifiers).render,
-        s.value.pure[?],
-      )
-    }
+      }
+      .labelErrors(label)
 
 }
 object inputs extends inputs
