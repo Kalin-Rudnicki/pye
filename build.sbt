0b401095457586cf05864cdabf623fda2f7ad40d
Kalin-Rudnicki <kalin.r.rudnicki@gmail.com>
2021-07-22 19:37:29 -0500

    Updated to new Executable paradigm

--- build.sbt
+++ build.sbt
@@ -19,7 +19,7 @@ lazy val `klib-webServer` =
     .in(file("klib-webServer"))
     .settings(
       name := "klib-webserver",
-      version := "0.10.0",
+      version := "1.0.0",
       unmanagedSourceDirectories in Compile +=
         baseDirectory.value / "shared" / "main" / "scala",
       libraryDependencies ++= Seq(
--- klib-webServer/jvm/src/main/scala/klib/webServer/package.scala
+++ klib-webServer/jvm/src/main/scala/klib/webServer/package.scala
@@ -26,82 +26,77 @@ package object webServer {
       defaultDbPath: String = "test-database.db",
       rbFile: Maybe[File] = None, // TODO (KR) : Make this work off of withRes, or include the rb file somehow
       withRes: ServerRes => Unit = _ => (),
-  ): Executable =
-    new Executable.ExecFromConf {
+  ): Executable = {
+    final class Conf(args: Seq[String]) extends Executable.Conf(args) {
+      val port: ScallopOption[Int] = opt[Int](default = defaultPort.someOpt)
+      val dbPath: ScallopOption[String] = opt[String](default = defaultDbPath.someOpt)
 
-      final class Conf(args: Seq[String]) extends ScallopConf(args) {
-        val port: ScallopOption[Int] = opt[Int](default = defaultPort.someOpt)
-        val dbPath: ScallopOption[String] = opt[String](default = defaultDbPath.someOpt)
+      verify()
+    }
+    object Conf extends Executable.ConfBuilder(new Conf(_))
 
-        verify()
-      }
+    Executable.fromConf(Conf) { (logger, conf) =>
+      val port = conf.port()
+      val dbFile = new File(conf.dbPath())
 
-      override def buildConf(args: Seq[String]): Conf =
-        new Conf(args)
+      val connectionFactory = ConnectionFactory.fromSqliteFile(dbFile)
+      val handler = new ServerHandler(
+        matcher = routeMatcher,
+        connectionFactory = connectionFactory,
+        logger = logger,
+      )
 
-      override def run(logger: Logger, conf: Conf): IO[Unit] = {
-        val port = conf.port()
-        val dbFile = new File(conf.dbPath())
+      for {
+        _ <- logger(
+          L(
+            L.log.info(s"Starting server on port: $port"),
+            L.log.info(s"Database path: $dbFile"),
+          ),
+        )
+        exists <- dbFile.exists.pure[IO]
+        _ <- {
+          if (exists) {
+            import scala.collection.mutable
+            import scala.sys.process._
+            val lb = mutable.ListBuffer[String]()
 
-        val connectionFactory = ConnectionFactory.fromSqliteFile(dbFile)
-        val handler = new ServerHandler(
-          matcher = routeMatcher,
+            for {
+              _ <- logger(L.log.info(s"Database already exists at: $dbFile"))
+              _ <- connectionFactory.openRunClose(schema.printDdl(s => if (!s.startsWith("--")) lb.append(s)).pure[Query])
+              list = lb.toList
+              _ <- logger(L.requireFlags("schema")(list.map(L.log.info)))
+              _ <- rbFile match {
+                case Some(rbFile) =>
+                  for {
+                    exists <- rbFile.exists.pure[IO]
+                    _ <-
+                      if (exists)
+                        ("ruby" :: rbFile.toString :: dbFile.toString :: list).!.pure[IO]
+                      else
+                        ().pure[IO]
+                  } yield ()
+                case None =>
+                  ().pure[IO]
+              }
+            } yield ()
+          } else
+            for {
+              _ <- logger(L.log.info(s"Creating database: $dbFile"))
+              _ <- connectionFactory.openRunClose(schema.create.pure[Query])
+            } yield ()
+        }
+        server <- new Server(port).pure[IO]
+        _ <- server.setHandler(handler).pure[IO]
+        _ <- server.start().pure[IO]
+
+        serverRes = ServerRes(
+          dbFile = dbFile,
           connectionFactory = connectionFactory,
           logger = logger,
         )
-
-        for {
-          _ <- logger(
-            L(
-              L.log.info(s"Starting server on port: $port"),
-              L.log.info(s"Database path: $dbFile"),
-            ),
-          )
-          exists <- dbFile.exists.pure[IO]
-          _ <- {
-            if (exists) {
-              import scala.collection.mutable
-              import scala.sys.process._
-              val lb = mutable.ListBuffer[String]()
-
-              for {
-                _ <- logger(L.log.info(s"Database already exists at: $dbFile"))
-                _ <- connectionFactory.openRunClose(schema.printDdl(s => if (!s.startsWith("--")) lb.append(s)).pure[Query])
-                list = lb.toList
-                _ <- logger(L.requireFlags("schema")(list.map(L.log.info)))
-                _ <- rbFile match {
-                  case Some(rbFile) =>
-                    for {
-                      exists <- rbFile.exists.pure[IO]
-                      _ <-
-                        if (exists)
-                          ("ruby" :: rbFile.toString :: dbFile.toString :: list).!.pure[IO]
-                        else
-                          ().pure[IO]
-                    } yield ()
-                  case None =>
-                    ().pure[IO]
-                }
-              } yield ()
-            } else
-              for {
-                _ <- logger(L.log.info(s"Creating database: $dbFile"))
-                _ <- connectionFactory.openRunClose(schema.create.pure[Query])
-              } yield ()
-          }
-          server <- new Server(port).pure[IO]
-          _ <- server.setHandler(handler).pure[IO]
-          _ <- server.start().pure[IO]
-
-          serverRes = ServerRes(
-            dbFile = dbFile,
-            connectionFactory = connectionFactory,
-            logger = logger,
-          )
-          _ = withRes(serverRes)
-        } yield ()
-      }
-
+        _ = withRes(serverRes)
+      } yield ()
     }
+  }
 
 }
