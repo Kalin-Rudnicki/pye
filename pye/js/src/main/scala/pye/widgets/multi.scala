008920ff51024411674f0435012477e5c04f192f
Kalin-Rudnicki <kalin.r.rudnicki@gmail.com>
2021-11-07 11:00:02 -0600

    Better handling inside listW

--- pye/js/src/main/scala/pye/widgets/multi.scala
+++ pye/js/src/main/scala/pye/widgets/multi.scala
@@ -22,49 +22,84 @@ trait multi {
           getState: () => List[S],
       ): AppliedWidget[List[V]] =
         new AppliedWidget[List[V]] {
-          private val children: Var[List[AppliedWidget[V]]] = Var(Nil)
+          private type State = Widget.ElementT \/ NonEmptyList[AppliedWidget[V]]
+
+          private val children: Var[State] =
+            Var(NonEmptyList.nel(Widget.placeholderSpan).left)
 
           override protected val valueImpl: IO[List[V]] =
             for {
               c <- IO { children.value }
-              v <- c.map(_.value).traverse
+              v <- c match {
+                case Right(c) => c.map(_.value).traverse.map(_.toList)
+                case Left(_)  => Nil.pure[IO]
+              }
             } yield v
 
           override protected val currentImpl: IO[Maybe[Widget.ElementT]] =
             for {
               c <- IO { children.value }
-              l <- c.map(_.current).traverse
-            } yield l.flatMap(_.map(_.toList).toList.flatten).toNel
+              e <- c match {
+                case Right(c) =>
+                  for {
+                    l <- c.map(_.current).traverse
+                    e1 = l.toList.flatMap(_.map(_.toList).toList.flatten).toNel
+                    e2 <- e1.toEA(Message("List widget is in bad state")).toIO
+                  } yield e2.some
+                case Left(e) => e.some.pure[IO]
+              }
+            } yield e
 
-          override protected val getElementsAndUpdateImpl: IO[Widget.ElementT] =
+          override protected val getElementsAndUpdateImpl: IO[Widget.ElementT] = {
+            def calcForSome(sl: NonEmptyList[S]): IO[(State, Widget.ElementT)] = {
+              def convertRaise(i: Int, r: Raise[S, A]): Raise[List[S], KeyedAction[Int, A]] =
+                r match {
+                  case update: Raise.UpdateState[S] =>
+                    update.mapUpdate[List[S]] { updateS => listS =>
+                      listS.zipWithIndex.map { case (s2, i2) => (i == i2) ? updateS(s2) | s2 }
+                    }
+                  case standard: Raise.Standard => standard
+                  case Raise.Action(action)     => Raise.Action(KeyedAction(i, action))
+                }
+
+              val aw =
+                sl.zipWithIndex.map {
+                  case (_, i) =>
+                    widget.captureReRender.convert(
+                      r => parentRaiseHandler._handleRaise(convertRaise(i, r)),
+                      // TODO (KR) : Not sure which to use (?)
+                      () => getState()(i),
+                      // () => s,
+                    )
+                }
+
+              for {
+                e <- aw.map(_.getElementsAndUpdate).traverse
+              } yield (
+                aw.right,
+                e.flatten,
+              )
+            }
+
+            def calcForNone: IO[(State, Widget.ElementT)] =
+              IO {
+                val e = NonEmptyList.nel(Widget.placeholderSpan)
+                (
+                  e.left,
+                  e,
+                )
+              }
+
             for {
               sl <- IO { getState() }
-              aw = sl.zipWithIndex.map {
-                case (_, i) =>
-                  widget.captureReRender.convert(
-                    r =>
-                      parentRaiseHandler._handleRaise(
-                        r match {
-                          case update: Raise.UpdateState[S] =>
-                            update.mapUpdate[List[S]] { updateS => listS =>
-                              listS.zipWithIndex.map { case (s2, i2) => (i == i2) ? updateS(s2) | s2 }
-                            }
-                          case standard: Raise.Standard => standard
-                          case Raise.Action(action)     => Raise.Action(KeyedAction(i, action))
-                        },
-                      ),
-                    // TODO (KR) : Not sure which to use (?)
-                    () => getState()(i),
-                    // () => s,
-                  )
+              tmp1 <- sl.toNel match {
+                case Some(sl) => calcForSome(sl)
+                case None     => calcForNone
               }
-              e1 <- aw.map(_.getElementsAndUpdate).traverse
-              e2 = e1.flatMap(_.toList).toNel match {
-                case Some(elems) => elems
-                case None        => NonEmptyList.nel(Widget.placeholderSpan)
-              }
-              _ <- children.value = aw
-            } yield e2
+              (newChildren, elems) = tmp1
+              _ <- children.value = newChildren
+            } yield elems
+          }
 
         }
 
